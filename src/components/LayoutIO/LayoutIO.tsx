3b4725a9a2a0e8b8e6b3c81334d4bb85ba56d860
Heyang Liu <lhypds@gmail.com>
2026-08-04 01:02:14 +0900

    Change icon style

--- src/components/LayoutIO/LayoutIO.tsx
+++ src/components/LayoutIO/LayoutIO.tsx
@@ -73,10 +73,8 @@ export default function LayoutIO({ layout, configs, onImport }: LayoutIOProps) {
   return (
     <div ref={wrapperRef} className={styles.wrapper} data-open={open}>
       <button type="button" className={styles.trigger} onClick={() => setOpen((v) => !v)}>
-        <svg className={styles.icon} viewBox="0 0 24 24">
-          <path d="M12 3v12" />
-          <path d="M7 10l5 5 5-5" />
-          <path d="M5 21h14" />
+        <svg className={styles.icon} viewBox="0 -960 960 960">
+          <path d="M510-570v-270h330v270H510ZM120-450v-390h330v390H120Zm390 330v-390h330v390H510Zm-390 0v-270h330v270H120Zm60-390h210v-270H180v270Zm390 330h210v-270H570v270Zm0-450h210v-150H570v150ZM180-180h210v-150H180v150Zm210-330Zm180-120Zm0 180ZM390-330Z" />
         </svg>
       </button>
       <div className={styles.dropdown}>
